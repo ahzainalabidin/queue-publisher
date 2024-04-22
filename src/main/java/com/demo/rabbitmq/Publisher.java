26093344809fda23fb8b4a35a2a0845092cbc232
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-04-22 09:36:15 +0800

    Publish message to queue

--- src/main/java/com/demo/rabbitmq/Publisher.java
+++ src/main/java/com/demo/rabbitmq/Publisher.java
@@ -1,13 +1,29 @@
 package com.demo.rabbitmq;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class Publisher {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Publisher.class, args);
-	}
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        String message = "First message from RabbitMQ";
+
+        channel.basicPublish("", "Queue-1", null, message.getBytes());
+        channel.close();
+        connection.close();
+
+    }
 
 }
