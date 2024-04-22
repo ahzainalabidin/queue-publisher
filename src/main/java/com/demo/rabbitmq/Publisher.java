c47fa6b9cdf6c5e916460b120b14a80a11450249
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-04-22 09:17:35 +0800

    Create a new Spring project

---
+++ src/main/java/com/demo/rabbitmq/Publisher.java
@@ -0,0 +1,13 @@
+package com.demo.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Publisher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Publisher.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/rabbitmq/PublisherTests.java
@@ -0,0 +1,13 @@
+package com.demo.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
